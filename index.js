540b561780e952007aa4df98cab0e3ff391fc3c1
John Beppu <johnb@ziprecruiter.com>
2017-12-24 06:49:47 -0800

    Update env vars to latest set

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Promise = require('bluebird')
 const Twit    = require('twit')
-const bittrex = require('bittrex')
 
 const Bot = require('./lib/bot')
 
 const opts = {
   twitter: {
-    key:    process.env.TWITTER_KEY,
-    secret: process.env.TWITTER_SECRET
+    key:         process.env.TWITTER_KEY,
+    secret:      process.env.TWITTER_SECRET,
+    token:       process.env.TWITTER_TOKEN,
+    tokenSecret: process.env.TWITTER_TOKEN_SECRET
   },
   bittrex: {
     key:    process.env.BITTREX_KEY,
@@ -16,6 +17,14 @@ const opts = {
 }
 
 const bot = new Bot(opts);
+
+if (process.env.BTC_SPEND) {
+  bot.btcSpend = parseFloat(process.env.BTC_SPEND)
+}
+
+if (process.env.ADJUSTMENT) {
+  bot.adjustment = parseFloat(process.env.ADJUSTMENT)
+}
 
 ;(async function() {
   let result = await bot.init();
